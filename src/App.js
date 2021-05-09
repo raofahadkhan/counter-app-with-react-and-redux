8b2c044e18f4b14cea867fa1cd377126922538f2
Rao Fahad <raofahad046@gmail.com>
2021-05-08 18:46:01 -0700

    ADD: buttons: increment,decrement,reset

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Counter from "./Components/Counter";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Counter App With Redux</h1>
+			<Counter />
 		</div>
 	);
 }
---
+++ src/Components/Counter.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Counter = () => {
+	return (
+		<div>
+			<div>Counter: 12</div>
+			<div>
+				<button onClick={() => {}}>Increment</button>
+				<button onClick={() => {}}>Decrement</button>
+				<button onClick={() => {}}>Reset</button>
+			</div>
+		</div>
+	);
+};
+
+export default Counter;
