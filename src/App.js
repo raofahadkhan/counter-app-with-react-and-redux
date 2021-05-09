3b086ad60d7ce0bd800997d9337571926b9ad766
Rao Fahad <raofahad046@gmail.com>
2021-05-08 18:51:32 -0700

    created a store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Counter from "./Components/Counter";
+import CounterDisplay from "./Components/CounterDisplay";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Counter App With Redux</h1>
 			<Counter />
+			<CounterDisplay />
 		</div>
 	);
 }
---
+++ src/Components/CounterDisplay.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CounterDisplay = () => {
+	return <div>Counter Display</div>;
+};
+
+export default CounterDisplay;
